99c98aec4443f8e832ed56ffeba5f35acef7bcbe
Rory <n/a>
2023-06-28 09:09:42 +0100

    refactor: tidy up DirectoryGenerator test

--- test/test_directory_generator.py
+++ test/test_directory_generator.py
@@ -3,9 +3,11 @@ import pytest
 from .test_helpers import *
 from app.directory_generator import DirectoryGenerator
 
+generator = DirectoryGenerator()
 
 def test_generates_path_including_year_and_quarter():
-    source_filepath = permanent_source_dir + 'test_file.txt'
-    desired_target_path = "/Users/rory/code/transfer_wizard_redux/test/media/target/2023/Q2/test_file.txt"
-    generated_path = DirectoryGenerator().generate_target_directory_path(source_filepath, target_dir)
+    filename = 'test_file.txt'
+    source_filepath = permanent_source_dir + filename
+    desired_target_path = target_dir + '2023/Q2/' + filename
+    generated_path = generator.generate_target_directory_path(source_filepath, target_dir)
     assert generated_path == desired_target_path

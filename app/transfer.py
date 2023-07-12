65a419992f1e1da5a456b9c0693ec6be1e2e7308
Rory <tine_lentos_0d@icloud.com>
2023-07-12 10:34:30 +0100

    refactor: prefer dependency injection

--- app/transfer.py
+++ app/transfer.py
@@ -7,11 +7,12 @@ from app.directory_creator import DirectoryCreator
 
 class Transfer:
 
-    def copy_files(self, source_files, target_directory):
+    def copy_files(self, source_files, target_directory,
+                   filepath_generator=FilepathGenerator, directory_creator=DirectoryCreator):
         for source_filepath in source_files:
-            target_filepath = FilepathGenerator(
+            target_filepath = filepath_generator(
                 source_filepath, target_directory).generate_target_filepath()
             target_directory = Path(target_filepath).parent
-            DirectoryCreator().create_directory(target_directory)
+            directory_creator(target_directory).create_directory()
             if target_filepath:
                 shutil.copy2(source_filepath, target_filepath)

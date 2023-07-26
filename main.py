6d8a09979c2efaa7e62441db04cbafc0c0a5cbc9
Rory <tine_lentos_0d@icloud.com>
2023-07-26 16:17:47 +0100

    feat: main now takes path args

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from pathlib import Path
+import argparse
+import os
+
 from app.scanner import Scanner
 from app.transfer import Transfer
 
 
-def main():
-    cwd = str(Path(__file__).parent)
-    source_directory = cwd + '/test/media/static_source/'
-    target_directory = cwd + '/test/media/target/'
+def main(source_directory, target_directory):
+    for path in [source_directory, target_directory]:
+        if not os.path.isdir(path):
+            raise FileNotFoundError(f'{path} is not a valid directory.')
+
+    source_directory = __sanitise_filepath(source_directory)
+    target_directory = __sanitise_filepath(target_directory)
+
     filepaths = Scanner().scan_dirs(source_directory)
     Transfer().copy_files(filepaths, target_directory)
 
 
+def __sanitise_filepath(filepath):
+    if filepath[-1] != '/':
+        filepath += '/'
+    return filepath
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='This program copies media files to an organised directory structure.')
+    parser.add_argument('-s', '--source', type=str, required=True)
+    parser.add_argument('-t', '--target', type=str, required=True)
+    args = parser.parse_args()
+    main(args.source, args.target)
--- test/test_main.py
+++ test/test_main.py
@@ -9,11 +9,29 @@ def teardown():
     clear_test_directories()
 
 
-def test_main():
+def test_copies_desired_file_to_generated_directory():
     file_path = str(Path(__file__).parent) + '/media/target/2023/Q2/a_file___1.jpeg'
 
     assert not os.path.isfile(file_path)
 
-    main()
+    main(static_source_directory, target_root_directory)
 
     assert os.path.isfile(file_path)
+
+
+def test_copies_file_when_paths_given_with_no_backslash_on_end():
+    file_path = str(Path(__file__).parent) + '/media/target/2023/Q2/a_file___1.jpeg'
+
+    assert not os.path.isfile(file_path)
+
+    source = static_source_directory[:-1]
+    target = target_root_directory[:-1]
+
+    main(source, target)
+
+    assert os.path.isfile(file_path)
+
+
+def test_throws_error_when_path_not_valid():
+    with pytest.raises(FileNotFoundError):
+        main(static_source_directory + 'jkjkjkjk', target_root_directory)

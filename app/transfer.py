1dc8425c8d152b202df15cd1a4d9cd9e62a932a7
Rory <tine_lentos_0d@icloud.com>
2023-09-12 12:50:37 +0100

    refactor: bring variable name in alignment with class

--- app/transfer.py
+++ app/transfer.py
@@ -7,8 +7,8 @@ from app.directory_manager import DirectoryManager
 class Transfer:
 
     def copy_files(self, source_filepath, target_filepath,
-                   directory_creator=DirectoryManager):
+                   directory_manager=DirectoryManager):
         target_directory = Path(target_filepath).parent
-        directory_creator().create_directory_if_not_exists(target_directory)  # perform a check to see if required?
+        directory_manager().create_directory_if_not_exists(target_directory)
         if target_filepath:
             shutil.copy2(source_filepath, target_filepath)

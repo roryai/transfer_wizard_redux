6a6de414667e263acf6f1f894c5e24b6afb038aa
Rory <tine_lentos_0d@icloud.com>
2023-09-19 11:52:07 +0100

    test: increase coverage for File.name_clash attribute

--- test/test_file.py
+++ test/test_file.py
@@ -22,6 +22,32 @@ def test_can_insert_and_read_record():
     assert file == retrieved_file
 
 
+def test_sets_name_clash_attribute_when_value_is_true():
+    file = File(source_filepath='/source',
+                target_filepath='/target',
+                size=1024,
+                name_clash=True)
+    file.save()
+
+    record = gateway.select_all()[0]
+    retrieved_file = File.init_from_record(record)
+
+    assert retrieved_file.name_clash is True
+
+
+def test_sets_name_clash_attribute_when_value_is_false():
+    file = File(source_filepath='/source',
+                target_filepath='/target',
+                size=1024,
+                name_clash=False)
+    file.save()
+
+    record = gateway.select_all()[0]
+    retrieved_file = File.init_from_record(record)
+
+    assert retrieved_file.name_clash is False
+
+
 def test_sets_copied_attribute_to_none_by_default():
     file = File(source_filepath='/source',
                 target_filepath='/target',
@@ -63,4 +89,3 @@ def test_sets_copied_attribute_when_value_is_false():
     retrieved_file = File.init_from_record(record)
 
     assert retrieved_file.copied is False
-

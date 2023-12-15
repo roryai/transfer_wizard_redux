0eae8ce47d107a2c5c5b3a45b0a68243eccea430
Rory <tine_lentos_0d@icloud.com>
2023-12-15 16:49:21 +0000

    feat: user can now provide -ext flag to CLI to see invalid extensions

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ import argparse
 from app.app_controller import AppController
 from app.db_initializer import DBInitializer
 
-
-def main(source_directory, target_directory):
-    DBInitializer().init_prod_database()
-    AppController(source_directory, target_directory).run()
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='This program copies media files to an organised directory structure.')
-    parser.add_argument('-s', '--source', type=str, required=True)
-    parser.add_argument('-t', '--target', type=str, required=True)
+        description='This program copies media files to an organised directory structure.\n'
+                    'Example usage:\n'
+                    'python main.py -s path/to/source -d path/to/destination  <-- To copy files '
+                    'from source to destination.\n'
+                    'python main.py -s path/to/source -ext  <-- To discover invalid extensions.')
+    parser.add_argument('-s', '--source', type=str, required=True, help='Source directory path.')
+    parser.add_argument('-d', '--destination', type=str, required=False, help='Destination directory path.')
+    parser.add_argument('-ext', '--extensions', action='store_true', default=False, required=False,
+                        help='Displays invalid extensions in source directory.')
     args = parser.parse_args()
-    main(args.source, args.target)
+
+    DBInitializer().init_prod_database()
+
+    if args.extensions:
+        AppController(args.source).display_invalid_extensions()
+    elif args.destination:
+        AppController(args.source).copy_files_from_source_to_destination(args.destination)
+    else:
+        error_message = "Must provide source flag (-s <directory path>) and either -ext flag or " \
+                        "-d flag (-d <directory path>"
+        raise argparse.ArgumentError(None, error_message)

9da53ad83efab558134d5ca1dbdcac00135d9aa0
Rory <tine_lentos_0d@icloud.com>
2024-01-10 16:49:38 +0000

    test: add test, add helper to fix mtime of file
    Now that mtime is fixed I can rely on the destination directory being predictable, removing the need to generate it in the helper methods using functionality duplicated from the class under test.

--- test/helpers.py
+++ test/helpers.py
@@ -83,6 +83,14 @@ def create_file(directory, filename):
     return create_file_with_data(directory, filename)
 
 
+def set_creation_time_of_source_file(source_filepath):
+    time_in_past = 1701639908  # 03/12/23
+    # setting mtime in past also sets birthtime to that time
+    os.utime(source_filepath, (time_in_past, time_in_past))
+    # destination directory is now predictable
+    return os.path.join(destination_root_directory, '2023/Q4/')
+
+
 def clear_database():
     FileGateway().wipe_database()
 
--- test/test_filepath_generator.py
+++ test/test_filepath_generator.py
@@ -11,16 +11,21 @@ def teardown():
 def test_generates_path_including_year_and_quarter():
     filename = 'test_file.jpeg'
     source_filepath = create_file(source_directory, filename)
-    generated_path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
-    destination_directory = get_destination_directory(source_filepath)
 
-    assert generated_path == destination_directory + filename
+    destination_directory = set_creation_time_of_source_file(source_filepath)
+
+    generated_destination_path = FilepathGenerator(source_filepath, destination_root_directory
+                                                   ).generate_destination_filepath()
+    expected_destination_path = os.path.join(destination_directory, filename)
+
+    assert expected_destination_path == generated_destination_path
 
 
 def test_adds_suffix_to_filename_if_there_is_a_name_clash_with_existing_file():
     filename = 'file_1.jpeg'
     source_filepath = create_file(source_directory, filename)
-    destination_directory = get_destination_directory(source_filepath)
+
+    destination_directory = set_creation_time_of_source_file(source_filepath)
 
     create_file_with_data(destination_directory, filename, 'Some original data')
 
@@ -29,10 +34,11 @@ def test_adds_suffix_to_filename_if_there_is_a_name_clash_with_existing_file():
     assert path == destination_directory + 'file_1___1.jpeg'
 
 
-def test_increments_number_suffix_if_name_clashes_with_file_with_suffix():
+def test_increments_number_suffix_if_name_clashes_with_file_that_already_has_suffix():
     filename = 'a_file___1.jpeg'
     source_filepath = create_file(source_directory, filename)
-    destination_directory = get_destination_directory(source_filepath)
+
+    destination_directory = set_creation_time_of_source_file(source_filepath)
 
     create_file_with_data(destination_directory, filename, 'Some original data')
 
@@ -41,12 +47,26 @@ def test_increments_number_suffix_if_name_clashes_with_file_with_suffix():
     assert path == destination_directory + 'a_file___2.jpeg'
 
 
-def test_returns_empty_path_if_generated_path_points_to_file_with_identical_name_suffix_and_size():
+def test_returns_none_if_generated_path_points_to_identical_file():
     filename = 'this_file.jpeg'
     source_filepath = create_file_with_data(source_directory, filename, 'Same data')
-    destination_directory = get_destination_directory(source_filepath)
+    destination_directory = set_creation_time_of_source_file(source_filepath)
 
+    create_file_with_data(destination_directory, filename, 'Same data')
+
+    path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
+
+    assert path is None
+
+
+def test_returns_none_if_second_path_generated_points_to_file_with_identical_name_suffix_and_size():
+    filename = 'this_file.jpeg'
+    source_filepath = create_file_with_data(source_directory, filename, 'Same data')
+    destination_directory = set_creation_time_of_source_file(source_filepath)
+
+    # source filepath has name clash with this filepath, so generated filename is incremented
     create_file_with_data(destination_directory, filename, 'Unique data')
+    # generated incremented filepath is identical, and files are same size/have same data
     create_file_with_data(destination_directory, 'this_file___1.jpeg', 'Same data')
 
     path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()

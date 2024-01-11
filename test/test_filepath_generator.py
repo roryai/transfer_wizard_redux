d3982ba75e570067a15e4108aa4a7aea275b41d0
Rory <tine_lentos_0d@icloud.com>
2024-01-11 11:22:28 +0000

    test: DRY test setup and class under test call to helper method

--- test/test_filepath_generator.py
+++ test/test_filepath_generator.py
@@ -5,70 +5,66 @@ from .helpers import *
 
 @pytest.fixture(autouse=True)
 def teardown():
+    yield
     clear_test_directories()
 
 
-def test_generates_path_including_year_and_quarter():
-    filename = 'test_file.jpeg'
-    source_filepath = create_file(source_directory, filename)
-
+def setup_and_run_test_class(filename='test_file.jpeg', run_func=True,
+                             source_data='default_source_data', dest_data='default_dest_data'):
+    func = create_file_with_data
+    source_filepath = func(source_directory, filename, source_data)
     destination_directory = static_destination_path(source_filepath)
-
+    func(destination_directory, filename, dest_data) if run_func else None
     generated_destination_path = FilepathGenerator(source_filepath, destination_root_directory
                                                    ).generate_destination_filepath()
+    return filename, source_filepath, destination_directory, generated_destination_path
+
+
+def test_generates_path_including_year_and_quarter():
+    filename, _, destination_directory, generated_destination_path = setup_and_run_test_class(
+        run_func=False)
+
     expected_destination_path = os.path.join(destination_directory, filename)
 
-    assert expected_destination_path == generated_destination_path
+    assert generated_destination_path == expected_destination_path
 
 
 def test_adds_suffix_to_filename_if_there_is_a_name_clash_with_existing_file():
-    filename = 'file_1.jpeg'
-    source_filepath = create_file(source_directory, filename)
+    filename, _, destination_directory, generated_destination_path = setup_and_run_test_class()
 
-    destination_directory = static_destination_path(source_filepath)
+    expected_destination_path = os.path.join(destination_directory, Path(filename).stem + '___1.jpeg')
 
-    create_file_with_data(destination_directory, filename, 'Some original data')
-
-    path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
-
-    assert path == destination_directory + 'file_1___1.jpeg'
+    assert generated_destination_path == expected_destination_path
 
 
 def test_increments_number_suffix_if_name_clashes_with_file_that_already_has_suffix():
-    filename = 'a_file___1.jpeg'
-    source_filepath = create_file(source_directory, filename)
+    _, _, destination_directory, generated_destination_path = setup_and_run_test_class(
+        filename='a_file___1.jpeg')
 
-    destination_directory = static_destination_path(source_filepath)
+    expected_destination_path = os.path.join(destination_directory, 'a_file___2.jpeg')
 
-    create_file_with_data(destination_directory, filename, 'Some original data')
-
-    path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
-
-    assert path == destination_directory + 'a_file___2.jpeg'
+    assert generated_destination_path == expected_destination_path
 
 
 def test_returns_none_if_generated_path_points_to_identical_file():
-    filename = 'this_file.jpeg'
-    source_filepath = create_file_with_data(source_directory, filename, 'Same data')
-    destination_directory = static_destination_path(source_filepath)
+    data = 'same data'
+    _, _, _, generated_destination_path = setup_and_run_test_class(
+        filename='a_file___1.jpeg', source_data=data, dest_data=data)
 
-    create_file_with_data(destination_directory, filename, 'Same data')
-
-    path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
-
-    assert path is None
+    assert generated_destination_path is None
 
 
-def test_returns_none_if_second_path_generated_points_to_file_with_identical_name_suffix_and_size():
-    filename = 'this_file.jpeg'
+def test_returns_none_if_second_path_generated_points_to_file_with_identical_name_and_suffix_and_size():
+    filename = 'test_file.jpeg'
     source_filepath = create_file_with_data(source_directory, filename, 'Same data')
     destination_directory = static_destination_path(source_filepath)
 
     # source filepath has name clash with this filepath, so generated filename is incremented
     create_file_with_data(destination_directory, filename, 'Unique data')
     # generated incremented filepath is identical, and files are same size/have same data
-    create_file_with_data(destination_directory, 'this_file___1.jpeg', 'Same data')
+    create_file_with_data(destination_directory, 'test_file___1.jpeg', 'Same data')
 
-    path = FilepathGenerator(source_filepath, destination_root_directory).generate_destination_filepath()
+    generated_destination_path = FilepathGenerator(source_filepath,
+                                                   destination_root_directory).generate_destination_filepath()
 
-    assert path is None
+    assert generated_destination_path is None

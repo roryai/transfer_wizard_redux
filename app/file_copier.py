9c824bba83073e4b6a0b9d287df5daf812afd089
Rory <tine_lentos_0d@icloud.com>
2024-01-12 23:37:28 +0000

    refactor: reorganise FileCopier

--- app/file_copier.py
+++ app/file_copier.py
@@ -14,30 +14,29 @@ class FileCopier:
         self.file_gateway = file_gateway()
 
     def copy_source_files_to_destination(self):
-        record_for_file_to_copy = self.file_gateway.select_one_where_copy_not_attempted()
-        while record_for_file_to_copy:
-            file = File.init_from_record(record_for_file_to_copy)
-            self.directory_manager.create_directory_if_not_exists(file.destination_directory())
-            self.__copy_file(file)
-            record_for_file_to_copy = self.file_gateway.select_one_where_copy_not_attempted()
+        while candidate_file_record := self.file_gateway.select_one_where_copy_not_attempted():
+            self.__prepare_destination_and_attempt_copy(candidate_file_record)
+
+    def __prepare_destination_and_attempt_copy(self, candidate_file_record):
+        file = File.init_from_record(candidate_file_record)
+        self.directory_manager.create_directory_if_not_exists(file.destination_directory())
+        self.__copy_file(file)
+        return self.file_gateway.select_one_where_copy_not_attempted()
 
     def __copy_file(self, file):
         shutil.copy2(file.source_filepath, file.destination_filepath)
-        if self.__file_copied(file):
-            self.__set_copy_success_and_log(file)
-        else:
-            self.__set_copy_failure_and_log(file)
+        self.__copy_success(file) if self.__confirm_file_copy(file) else self.__copy_failure(file)
         self.file_gateway.update_copied(file.copied, file.copy_attempted, file.source_filepath)
 
-    def __set_copy_failure_and_log(self, file):
-        file.copied = False
-        file.copy_attempted = True
-        Logger().log_unsuccessful_copy(file.source_filepath, file.destination_filepath)
+    def __confirm_file_copy(self, file):
+        return Path(file.destination_filepath).is_file()
 
-    def __set_copy_success_and_log(self, file):
+    def __copy_success(self, file):
         file.copied = True
         file.copy_attempted = True
         Logger().log_successful_copy(file.source_filepath, file.destination_filepath)
 
-    def __file_copied(self, file):
-        return Path(file.destination_filepath).is_file()
+    def __copy_failure(self, file):
+        file.copied = False
+        file.copy_attempted = True
+        Logger().log_unsuccessful_copy(file.source_filepath, file.destination_filepath)

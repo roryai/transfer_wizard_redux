f29807753bd40aff7d6ef0efa932ada3ff135e51
Rory <tine_lentos_0d@icloud.com>
2025-04-30 18:36:48 +0100

    refactor: make base directory path platform agnostic

--- test/fixtures/main_integration_fixtures.py
+++ test/fixtures/main_integration_fixtures.py
@@ -1,23 +1,25 @@
 from datetime import date
-import os
 from pathlib import Path
 import shutil
 
-from test.helpers import (pytest, construct_path, destination_root_directory, source_directory)
+from test.helpers import (pytest, construct_path, source_directory)
 from app.mode_flags import ModeFlagsMeta
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+static_media_directory = BASE_DIR / 'test_resources' / 'static_media'
+source_root_directory = BASE_DIR / 'test_resources' / 'source'
+destination_root_directory = BASE_DIR / 'test_resources' / 'destination'
 
 jpg_filename = 'RRY01936.JPG'
 raf_filename = 'RRY01936.RAF'
 video_filename = 'RRY01937.MOV'
-home_directory = os.path.expanduser("~")
-static_media_directory = '/code/transfer_wizard_redux/test/test_resources/static_media/'
+
 this_year = str(date.today().year)
-default_mode_expected_destination_jpg_file_path = construct_path(destination_root_directory, '2025/Q2',
-                                                                 jpg_filename)
-default_mode_expected_destination_raf_file_path = construct_path(destination_root_directory, '2025/Q2',
-                                                                 raf_filename)
-default_mode_expected_destination_video_file_path = construct_path(destination_root_directory, '2025/Q2',
-                                                                   video_filename)
+
+default_mode_expected_destination_jpg_file_path = construct_path(destination_root_directory, '2025/Q2', jpg_filename)
+default_mode_expected_destination_raf_file_path = construct_path(destination_root_directory, '2025/Q2', raf_filename)
+default_mode_expected_destination_video_file_path = construct_path(destination_root_directory, '2025/Q2', video_filename)
+
 year_mode_expected_destination_jpg_file_path = construct_path(destination_root_directory, '2025', jpg_filename)
 year_mode_expected_destination_raf_file_path = construct_path(destination_root_directory, '2025', raf_filename)
 year_mode_expected_destination_video_file_path = construct_path(destination_root_directory, '2025', video_filename)
@@ -27,23 +29,18 @@ year_mode_expected_destination_video_file_path = construct_path(destination_root
 def prepare_test_resources(monkeypatch):
     ModeFlagsMeta._instance = {}
     monkeypatch.setattr('builtins.input', lambda: 'y')
+
     nested_directory = construct_path(source_directory, 'nest')
     Path(nested_directory).mkdir(parents=True, exist_ok=True)
-    jpg_filepath = \
-        f'{home_directory}{static_media_directory}{jpg_filename}'
-    raf_filepath = \
-        f'{home_directory}{static_media_directory}{raf_filename}'
-    video_filepath = \
-        f'{home_directory}{static_media_directory}{video_filename}'
-    # real photo and video required so that metadata can be read
-    for path in [jpg_filepath, raf_filepath, video_filepath]:
-        shutil.copy2(path, source_directory)
+
+    for filename in [jpg_filename, raf_filename, video_filename]:
+        shutil.copy2(static_media_directory / filename, source_directory)
 
 
 @pytest.fixture
 def year_mode_expected_logfile_contents():
-    return f"""Source root directory: {home_directory}/code/transfer_wizard_redux/test/test_resources/source
-Destination root directory: {home_directory}/code/transfer_wizard_redux/test/test_resources/destination
+    return f"""Source root directory: {source_root_directory}
+Destination root directory: {destination_root_directory}
 
 Discovered         To be copied       Duplicate          Name clash
 Count   Size       Count   Size       Count   Size       Count   Size
@@ -57,9 +54,9 @@ Total to be copied:
 3 files
 75.52MB
 
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{raf_filename} copied to {year_mode_expected_destination_raf_file_path}
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{jpg_filename} copied to {year_mode_expected_destination_jpg_file_path}
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{video_filename} copied to {year_mode_expected_destination_video_file_path}
+Copy succeeded: {source_root_directory / raf_filename} copied to {year_mode_expected_destination_raf_file_path}
+Copy succeeded: {source_root_directory / jpg_filename} copied to {year_mode_expected_destination_jpg_file_path}
+Copy succeeded: {source_root_directory / video_filename} copied to {year_mode_expected_destination_video_file_path}
 
 3 files copied successfully
 0 files failed to copy
@@ -70,8 +67,8 @@ Errors:
 
 @pytest.fixture
 def media_only_copy_expected_logfile_contents():
-    return f"""Source root directory: {home_directory}/code/transfer_wizard_redux/test/test_resources/source
-Destination root directory: {home_directory}/code/transfer_wizard_redux/test/test_resources/destination
+    return f"""Source root directory: {source_root_directory}
+Destination root directory: {destination_root_directory}
 
 Discovered         To be copied       Duplicate          Name clash
 Count   Size       Count   Size       Count   Size       Count   Size
@@ -85,9 +82,9 @@ Total to be copied:
 3 files
 75.52MB
 
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{raf_filename} copied to {default_mode_expected_destination_raf_file_path}
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{jpg_filename} copied to {default_mode_expected_destination_jpg_file_path}
-Copy succeeded: {home_directory}/code/transfer_wizard_redux/test/test_resources/source/{video_filename} copied to {default_mode_expected_destination_video_file_path}
+Copy succeeded: {source_root_directory / raf_filename} copied to {default_mode_expected_destination_raf_file_path}
+Copy succeeded: {source_root_directory / jpg_filename} copied to {default_mode_expected_destination_jpg_file_path}
+Copy succeeded: {source_root_directory / video_filename} copied to {default_mode_expected_destination_video_file_path}
 
 3 files copied successfully
 0 files failed to copy

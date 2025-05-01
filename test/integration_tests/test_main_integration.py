1627c46fa5f26b41357762cc8195ce2cf62bec09
Rory <tine_lentos_0d@icloud.com>
2025-05-01 14:35:31 +0100

    test: make copy succeeded assertions order agnostic

--- test/integration_tests/test_main_integration.py
+++ test/integration_tests/test_main_integration.py
@@ -12,26 +12,95 @@ def teardown():
     cleanup()
 
 
-def test_year_mode_happy_path(prepare_test_resources, set_year_mode_args, year_mode_expected_logfile_contents):
+def test_year_mode_happy_path_file_creation(prepare_test_resources, set_year_mode_args):
     main()
 
     assert Path(year_mode_expected_destination_jpg_file_path).is_file()
     assert Path(year_mode_expected_destination_raf_file_path).is_file()
     assert Path(year_mode_expected_destination_video_file_path).is_file()
 
+
+def test_year_mode_happy_path_log_output(prepare_test_resources, set_year_mode_args):
+    main()
+
     with open(Logger().log_file_path, 'r') as file:
         contents = file.read()
-        assert contents == year_mode_expected_logfile_contents
+
+    assert contents.startswith(
+        f"""Source root directory: {source_root_directory}
+Destination root directory: {destination_root_directory}
+
+Discovered         To be copied       Duplicate          Name clash
+Count   Size       Count   Size       Count   Size       Count   Size
+____________________________________________________________________________
+3       75.52MB    3       75.52MB    0       0.0MB      0       0.0MB      
+
+Duplicates will not be copied
+Name clash files will be copied with a unique suffix
+
+Total to be copied:
+3 files
+75.52MB
+"""
+    )
+
+    expected_success_lines = [
+        f"Copy succeeded: {source_root_directory / raf_filename} copied to {year_mode_expected_destination_raf_file_path}",
+        f"Copy succeeded: {source_root_directory / jpg_filename} copied to {year_mode_expected_destination_jpg_file_path}",
+        f"Copy succeeded: {source_root_directory / video_filename} copied to {year_mode_expected_destination_video_file_path}",
+    ]
+
+    for line in expected_success_lines:
+        assert line in contents, f"Expected success line not found: {line}"
+
+    # --- Check footer ---
+    assert """3 files copied successfully
+0 files failed to copy
+Errors:""" in contents
 
 
-def test_default_mode_happy_path(media_only_copy_expected_logfile_contents,
-                                 prepare_test_resources, set_default_copy_args):
+def test_default_mode_happy_path(media_only_copy_expected_logfile_contents, set_default_copy_args):
     main()
 
     assert Path(default_mode_expected_destination_jpg_file_path).is_file()
     assert Path(default_mode_expected_destination_raf_file_path).is_file()
     assert Path(default_mode_expected_destination_video_file_path).is_file()
 
+
+def test_default_mode_happy_path_log_output(prepare_test_resources, set_default_copy_args):
+    main()
+
     with open(Logger().log_file_path, 'r') as file:
         contents = file.read()
-        assert contents == media_only_copy_expected_logfile_contents
+
+    assert contents.startswith(
+        f"""Source root directory: {source_root_directory}
+Destination root directory: {destination_root_directory}
+
+Discovered         To be copied       Duplicate          Name clash
+Count   Size       Count   Size       Count   Size       Count   Size
+____________________________________________________________________________
+3       75.52MB    3       75.52MB    0       0.0MB      0       0.0MB      
+
+Duplicates will not be copied
+Name clash files will be copied with a unique suffix
+
+Total to be copied:
+3 files
+75.52MB
+"""
+    )
+
+    expected_success_lines = [
+        f"Copy succeeded: {source_root_directory / raf_filename} copied to {default_mode_expected_destination_raf_file_path}",
+        f"Copy succeeded: {source_root_directory / jpg_filename} copied to {default_mode_expected_destination_jpg_file_path}",
+        f"{source_root_directory / video_filename} copied to {default_mode_expected_destination_video_file_path}",
+    ]
+
+    for line in expected_success_lines:
+        assert line in contents, f"Expected success line not found: {line}"
+
+    # --- Check footer ---
+    assert """3 files copied successfully
+0 files failed to copy
+Errors:""" in contents
